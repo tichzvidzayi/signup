e5942dcfbf072d12ebae1963243f5c6473cd7430
Tich Zvidzayi <tzvidzie@gmail.com>
2024-06-04 11:52:10 +0200

    Update Home.jsx

--- frontend/portal/src/components/Home.jsx
+++ frontend/portal/src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     <div className="min-h-screen bg-gray-200 flex flex-col">
       <header className="bg-black text-white py-4">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-3xl font-bold">Apollo Projects</h1>
+          <h1 className="text-3xl font-bold">Tich Projects</h1>
           <nav>
             <ul className="flex space-x-4">
             </ul>
@@ -15,7 +15,7 @@ function Home() {
         <section className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url(https://source.unsplash.com/random)' }}>
           <div className="container mx-auto h-full flex items-center justify-center">
             <div className="text-center text-white">
-              <h1 className="text-6xl font-bold mb-4">Welcome to Apollo Projects</h1>
+              <h1 className="text-6xl font-bold mb-4">Welcome to Tich Projects</h1>
               <span className=" bg-black ">..refresh to change background image...</span>
             </div>
           </div>
